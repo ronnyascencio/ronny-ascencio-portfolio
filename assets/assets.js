4081c567bf3c589c2f74b0c4c1b6d477ef2def2e
Ronny Ascencio <ronny.ascencio@icloud.com>
2025-02-22 11:26:45 -0500

    iitializing web dev portafolio

---
+++ assets/assets.js
@@ -0,0 +1,26 @@
+import adobe_logo from "./adobe-2.svg";
+import fastapi_logo from "./fastapi-1.svg";
+import nuke_logo from "./foundry-nuke.svg";
+import git_logo from "./git-icon.svg";
+import github_logo from "./github-icon-1.svg";
+import linux_logo from "./linux-tux.svg";
+import maya_logo from "./maya-2017.svg";
+import python_logo from "./python.png";
+import react_logo from "./react.png";
+import renderman_logo from "./renderman-logo.svg";
+import mongo_logo from "./mongodb-icon-1.svg";
+import sql_logo from "./postgresql.svg";
+export const assets = {
+  adobe_logo,
+  fastapi_logo,
+  nuke_logo,
+  git_logo,
+  github_logo,
+  linux_logo,
+  maya_logo,
+  python_logo,
+  react_logo,
+  renderman_logo,
+  mongo_logo,
+  sql_logo,
+};
