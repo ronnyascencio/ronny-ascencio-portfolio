48705e0abe72e393b33601d793f4022b960981fd
Ronny Ascencio <ronny.ascencio@icloud.com>
2025-02-22 21:02:12 -0500

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Exporta archivos estáticos
+  images: {
+    unoptimized: true, // Evita problemas con imágenes en GitHub Pages
+  },
+  basePath: "/ronny-ascencio-portfolio", // Nombre de tu repositorio en GitHub
+  assetPrefix: "/ronny-ascencio-portfolio/", // Asegura que los assets se carguen bien
+};
 
 export default nextConfig;
